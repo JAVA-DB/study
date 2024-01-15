1deaefa8fc1c76dc59774c57ca25df38184b2003
JAVA-DB <chlwoaud1234@gmail.com>
2024-01-16 00:06:02 +0900

    del dir

--- security/src/main/java/com/springboot/security/data/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.springboot.security.data.repository;
-
-import com.springboot.security.data.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    User getByUid(String id);
-}
